665194287208a166457dbad8768123f1ee4e567c
Michal <mmarciniak299@gmail.com>
2024-02-06 20:50:52 +0100

    todo-comments config

--- init.lua
+++ init.lua
@@ -70,6 +70,14 @@ return {
           },
         }
       end,
+    },
+    {
+      "folke/todo-comments.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        require('todo-comments').setup {}
+      end,
+      event = "User AstroFile",
     }
   },
   lsp = {
